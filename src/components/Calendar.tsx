5eba03d46b4ccf266bdff00c5d5f4a08b7fa09b0
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-08-11 17:36:01 -0300

    Learning about plugins usage

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,12 +1,21 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"
 
 export default function Calendar() {
+    const handleDateClick = (arg: any) => {
+        console.log(arg.dateStr)
+    }
+
     return (
         <FullCalendar
-            plugins={[dayGridPlugin]}
+            plugins={[dayGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
-            
+            events={[
+                { title: 'event 1', date: '2024-08-12' },
+                { title: 'event 2', date: '2024-08-20' }
+            ]}
+            dateClick={handleDateClick}
         />
     )
 }
