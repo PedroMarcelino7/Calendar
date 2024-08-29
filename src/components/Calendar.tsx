e34e5b803741ada41ebee237f62f9adecd95db61
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-08-29 11:09:09 -0300

    Commiting changes before deploy

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,6 +2,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"
 import { EventClickArg } from '@fullcalendar/core'
+import { useState } from 'react'
 
 interface Activity {
     ACTIVITY_CREATE_AT: string,
@@ -21,19 +22,38 @@ interface Props {
 }
 
 export default function Calendar({ activities, handleDateClick }: Props) {
+    const [showChangeStatus, setShowChangeStatus] = useState<{ top: number, left: number } | null>(null)
+
+    const handleEditStatus = (activityId: string, x: number, y: number) => {
+        setShowChangeStatus({ top: x, left: y })
+        console.log(activityId)
+    }
 
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            events={activities.map((activity) => ({
-                title: activity.ACTIVITY_TITLE,
-                date: activity.ACTIVITY_DATE,
-                color: `${activity.ACTIVITY_STATUS === 1 ? 'orange' : (activity.ACTIVITY_STATUS === 2 ? 'blue' : 'green')}`,
-                id: activity.ACTIVITY_ID.toString()
-            }))}
-            eventClick={(event: EventClickArg) => console.log(event.event.id)}
-            dateClick={handleDateClick}
-        />
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                events={activities.map((activity) => ({
+                    title: activity.ACTIVITY_TITLE,
+                    date: activity.ACTIVITY_DATE,
+                    color: `${activity.ACTIVITY_STATUS === 1 ? 'orange' : (activity.ACTIVITY_STATUS === 2 ? 'blue' : 'green')}`,
+                    id: activity.ACTIVITY_ID.toString()
+                }))}
+                eventClick={(event: EventClickArg) => handleEditStatus(event.event.id, event.jsEvent.clientX, event.jsEvent.clientY)}
+                dateClick={handleDateClick}
+            />
+
+            {showChangeStatus &&
+                <div style={{
+                    position: 'absolute',
+                    top: showChangeStatus.top,
+                    left: showChangeStatus.left,
+                    backgroundColor: 'red'
+                }}>
+                    teste
+                </div>
+            }
+        </>
     )
 }
